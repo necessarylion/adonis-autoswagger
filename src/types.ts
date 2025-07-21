b78b750842aac9205f5911697b67da8892ff45e4
necessarylion <aj.zinkyaw@gmail.com>
2025-07-21 17:01:52 +0700

    fix type issue on adonis route

--- src/types.ts
+++ src/types.ts
@@ -60,9 +60,7 @@ export interface AdonisRoute {
   handler?: string | v6Handler;
 }
 
-export interface AdonisRoutes {
-  root: AdonisRoute[];
-}
+export type AdonisRoutes = any
 
 export const standardTypes = [
   "string",
